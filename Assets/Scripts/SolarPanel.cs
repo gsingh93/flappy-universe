8c4a2af3662699763b68e3b6d8fd17483ed53d6f
Gulshan Singh <gulshan@umich.edu>
2014-02-22 17:50:45 -0500

    Make solar panel resources depend on distance to star

--- Assets/Scripts/SolarPanel.cs
+++ Assets/Scripts/SolarPanel.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class SolarPanel : Resource {
+
 	public override int harvestResources(Planet p, bool dryRun) {
-		return 10;
+		float distance = Vector3.Distance(p.transform.position, p.transform.parent.transform.position);
+		return (int) (100 / distance);
 	}
 }
