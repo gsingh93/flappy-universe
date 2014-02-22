18ec1a3e23f7a5289c7da6e50490384bf6832509
Jacob Hash <jhash@umich.edu>
2014-02-22 01:15:15 -0500

    Our Solar System Set Up!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	public Hashtable resources;
+	public List<Planet> planets;
+
+	// Use this for initialization
+	private void Start () {
+		resources = new Hashtable ();
+		resources.Add ("Energy", 100);
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	
+	}
+}
