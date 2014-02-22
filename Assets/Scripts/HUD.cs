5fbec5869dcbe100cd0706c398fd5f7298fb76ff
Connor Stack <cstack@umich.edu>
2014-02-22 00:12:14 -0500

    Zoom in on selected object.

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -3,6 +3,23 @@ using System.Collections;
 
 public class HUD : MonoBehaviour {
 	public SelectableObject selectedObject;
+	public float u = 0.5f;
+
+	private Vector3 startPosition;
+	private Vector3 targetPosition;
+
+	private void Start() {
+		startPosition = transform.position;
+	}
+
+	private void FixedUpdate() {
+		if (selectedObject != null) {
+			targetPosition = selectedObject.transform.position + new Vector3(0, 0, -5f);
+		} else {
+			targetPosition = startPosition;
+		}
+		transform.position = (1 - u) * transform.position + u * targetPosition;
+	}
 
 	private void OnGUI () {
 		if (selectedObject != null) {
