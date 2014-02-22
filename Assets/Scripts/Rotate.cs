ee2612ca58a2b4991653b9ca82ea89d1e5807d6f
Gulshan Singh <gulshan@umich.edu>
2014-02-22 03:01:29 -0500

    Rotate planets and stars

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public float speed = 0.5f;
+
+	void Update() {
+		transform.localEulerAngles += speed * Vector3.forward;
+	}
+}
