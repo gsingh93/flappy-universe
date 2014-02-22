e20d031ff166b6750a6dd68e4868b1208538de96
Jacob Hash <jhash@umich.edu>
2014-02-21 21:57:45 -0500

    Planet Script Started

---
+++ Assets/Planet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+	public string planetName;
+
+	// Use this for initialization
+	private void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		Debug.Log (planetName + " Clicked");
+
+
+	}
+
+	void OnGUI () {
+		GUI.Box (new Rect (10, 10, 200, 150), "Loader Menu");
+
+		if (GUI.Button (new Rect (20, 40, 150, 20), "Build Mine (30)")) {
+			Debug.Log ("Building Mine On Planet " + planetName);
+		}
+	}
+
+}
