bb9d43ae7d940073462869ab5f65e8ca60d29137
Connor Stack <cstack@umich.edu>
2014-02-22 00:00:32 -0500

    Merge those scenes. Change camera angle and planet speed.

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -8,16 +8,24 @@ public class Planet : SelectableObject {
 	public float speed;
 	public float radius;
 	
-	public const float maxSpeed = 0.5f;
-	public const float minSpeed = 1f;
+	public const float maxSpeed = 0.01f;
+	public const float minSpeed = 0.2f;
 
 	public Texture[] textures;
 
 	public Vector2 origin;
 
-	private void Start() {
+	private GameObject parent;
+
+	new private void Start() {
 		base.Start ();
+		parent = transform.parent.gameObject;
 		renderer.material.mainTexture = textures[Random.Range(0, textures.Length)];
+	}
+
+	private void Update() {
+		float angle = Time.time * speed;
+		transform.position = parent.transform.position + radius * new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
 	}
 
 	#region implemented abstract members of SelectableObject
