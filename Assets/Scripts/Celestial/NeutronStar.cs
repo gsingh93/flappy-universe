96e26cadb4ed424a3c4f058978c80e212bb65a92
Jacob Hash <jhash@umich.edu>
2014-02-22 23:18:24 -0500

    All Star Transitions Done!

---
+++ Assets/Scripts/Celestial/NeutronStar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeutronStar : Celestial {
+	
+	new private void Start () {
+		base.Start ();
+		
+		stateType = "Neutron Star";
+		starLabelOffset = 2.33f;
+	}
+	
+	
+}
