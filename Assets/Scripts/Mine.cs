a3de26f82a13afb7ff90785b27cf17381a6aee17
Connor Stack <cstack@umich.edu>
2014-02-22 23:12:05 -0500

    Make mining more powerful.

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Mine : Resource {
-	private const int numResources = 20;
+	private const int numResources = 30;
 	public override int harvestResources(Planet p, bool dryRun) {
 		int resources = Mathf.Min(numResources, p.totalResources);
 		if (!dryRun) {
