547c45084112bb9993e55e1a27b8420baa96de1c
Connor Stack <cstack@umich.edu>
2014-02-22 21:06:46 -0500

    Seperate generating of our solar system and random solar system.

--- Assets/Scripts/SolarSystem.cs
+++ Assets/Scripts/SolarSystem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SolarSystem : MonoBehaviour {
 
@@ -8,53 +9,60 @@ public class SolarSystem : MonoBehaviour {
 	public Celestial[] celestialPrefabs;
 
 	public Celestial celestial;
+	public List<Planet> planets;
 
 	public const int minPlanets = 6;
 	public const int maxPlanets = 10;
 
 	private int numPlanets;
-	private string[] ourPlanets;
 
-	public void GenerateOurSolarSystem() {
-	}
-
-	public void GenerateRandomSolarSystem() {
-	}
-	
 	private void Start() {
 		if (ourSolarSystem) {
 			GenerateOurSolarSystem();
 		} else {
 			GenerateRandomSolarSystem();
 		}
+	}
+	
+	public void GenerateOurSolarSystem() {
+		celestial = Instantiate (celestialPrefabs [0]) as Celestial;
+		
+		string[] ourPlanets = new string[] {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+		numPlanets = ourPlanets.Length;
+		
+		for (int i = 1; i <= numPlanets; i++) {
+			Planet planet = Instantiate(planetPrefab) as Planet;
+			Revolve revolution = planet.GetComponent<Revolve>();
+			planet.transform.parent = transform;
+			revolution.speed = Random.value * (Planet.maxSpeed - Planet.minSpeed) + Planet.minSpeed;
+			revolution.radius = i * 2;
+			
+			if (i == 3) {
+				Camera.main.GetComponent<Player> ().claimPlanet(planet);
+			}
+			
+			planet.planetName = ourPlanets[i-1];
+			planet.gameObject.name = ourPlanets[i-1];
+			
+			planets.Add(planet);
+		}
+	}
+	
+	public void GenerateRandomSolarSystem() {
 		int celestialIndex = (int) (Random.value * celestialPrefabs.Length);
 		celestial = Instantiate (celestialPrefabs[celestialIndex]) as Celestial;
 		celestial.transform.position = transform.position;
-
-		ourPlanets = new string[] {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-		if (ourSolarSystem) 
-			numPlanets = ourPlanets.Length;
-		else
-			numPlanets = Random.Range(minPlanets, maxPlanets);
-
+		
+		numPlanets = Random.Range(minPlanets, maxPlanets);
+		
 		for (int i = 1; i <= numPlanets; i++) {
 			Planet planet = Instantiate(planetPrefab) as Planet;
 			planet.transform.parent = transform;
 			Revolve revolution = planet.GetComponent<Revolve>();
 			revolution.speed = Random.value * (Planet.maxSpeed - Planet.minSpeed) + Planet.minSpeed;
 			revolution.radius = i * 2;
-
-			if (ourSolarSystem && i < 9) {
-				if (ourPlanets[i-1] == "Earth") {
-					Camera.main.GetComponent<Player> ().claimPlanet(planet);
-				}
-				planet.planetName = ourPlanets[i-1];
-				planet.gameObject.name = ourPlanets[i-1];
-			}
+			
+			planets.Add(planet);
 		}
-	}
-	
-	private void Update() {
-
 	}
 }
